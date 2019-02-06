54953b3b02b186b8dd591103914067d197d7c4f5
Soumya Deb <soumyadeb1809@gmail.com>
2019-02-06 10:30:53 +0530

    Initial commit

---
+++ app/src/main/java/com/soumyadeb/scanbotdemo/AppConstants.java
@@ -0,0 +1,20 @@
+package com.soumyadeb.scanbotdemo;
+
+public class AppConstants {
+
+    private final String licenseKey =
+            "pO7JaygLnBgCDCm/9HB4y+oA5hOXbK" +
+                    "l9HrxHKwk/CIMGSqnEqSppyH9ZQM+F" +
+                    "c1F1wwBDz7gKMRSJh7IDVFqyoLWm6q" +
+                    "Zv23i6mtFWQ8SO8hLKqFzBz5mw6pbU" +
+                    "d/3t1zt5I59zoPZW4vtZBs6Pey4yjx" +
+                    "61LiJBsnBJNQ5BNOhbWOjzb6rqyjtp" +
+                    "+K2gJDfp1QNRPWkaweP0Jx7T3MlzD9" +
+                    "J/KnUYXmgRFLtr+CUXO7CzLb1ADWl/" +
+                    "YqIoW30j0heAkyn/IaPBWA9kn6Gfbo" +
+                    "kzSriZbX0paXctHDk9OfkqOPoR3U//" +
+                    "RSv8feiDquMc7slrON8QI6bex8HH6x" +
+                    "athunxwCWlfg==\nU2NhbmJvdFNESw" +
+                    "pjb20uc291bXlhZGViLnNjYW5ib3Rk" +
+                    "ZW1vCjE1NTIwODk1OTkKNTkwCjM=\n";
+}
---
+++ app/src/main/java/com/soumyadeb/scanbotdemo/ScanBotDemo.java
@@ -0,0 +1,12 @@
+package com.soumyadeb.scanbotdemo;
+
+import android.app.Application;
+
+public class ScanBotDemo extends Application {
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+    }
+}
